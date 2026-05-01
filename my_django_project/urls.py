9ae6ce1e2662e80b361b643a8578dbe617ed7084
anatoliy-zhuk <af3557771@gmail.com>
2026-05-01 23:24:01 +0300

    Add main URL configuration

---
+++ my_django_project/urls.py
@@ -0,0 +1,10 @@
+# my_django_project/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('tns_electricity.urls')),  # подключаем приложение
+
+]
