cd6d24c0437feb5c69b5e436bb254055e8107e50
anatoliy-zhuk <af3557771@gmail.com>
2026-05-04 16:46:23 +0300

    Update URLs: add custom admin route and standard admin

--- my_django_project/urls.py
+++ my_django_project/urls.py
@@ -2,9 +2,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from tns_electricity.admin import admin_site
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('tns_electricity.urls')),  # подключаем приложение
-
+    path('admin/', admin.site.urls),           # стандартная админка
+    path('custom-admin/', admin_site.urls),    # ваша кастомная админка с кнопками
+    path('', include('tns_electricity.urls')),
 ]
